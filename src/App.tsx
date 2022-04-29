db2287a58da6666f0af8613441f73b1164595926
Eric <ericthedev@gmail.com>
2022-04-29 11:07:02 -0400

    final template

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
-function App () {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +10,12 @@ function App () {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React Again
         </a>
       </header>
     </div>
-  )
-};
+  );
+}
 
-export default App
+export default App;
