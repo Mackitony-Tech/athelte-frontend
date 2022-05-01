185fe4ebd676cb09778c4723185d0eab8131a1ee
Eric <ericthedev@gmail.com>
2022-05-01 11:41:40 -0400

    navigation buttons

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import Button from "react-bootstrap/esm/Button";
 
 export default function Navigation() {
   return (
     <>
       <div>
-        <ul>
-          <li>Home</li>
-          <li>Blog</li>
-          <li>Admin</li>
-        </ul>
+        <div>Kamwosor Athletics camp</div>
+        <Button variant="light" href="/">
+          Home
+        </Button>
+        <Button variant="light" href="/blog">
+          Blog
+        </Button>
+        <Button variant="light" href="/admin">
+          Admin
+        </Button>
       </div>
     </>
   );
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <>
       <Navigation />
-      <div>Home</div>
     </>
   );
 }
