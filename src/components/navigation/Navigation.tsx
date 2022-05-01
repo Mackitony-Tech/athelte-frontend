b109d9fc44723cf8196a7403fbdabd54309fb09c
Eric <ericthedev@gmail.com>
2022-05-01 09:52:49 -0400

    Routing

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Blog</li>
+          <li>Admin</li>
+        </ul>
+      </div>
+    </>
+  );
+}
