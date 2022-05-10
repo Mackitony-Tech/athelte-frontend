aff23b9e6ff93f78c74b47a9077718fe17423993
Eric <ericthedev@gmail.com>
2022-05-10 12:04:00 -0400

    fix(nav): Add side nav

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -7,28 +7,45 @@ import {
   HeaderName,
   HeaderNavigation,
   HeaderMenuItem,
+  HeaderMenu,
+  HeaderSideNavItems,
+  SideNav,
+  SideNavItems,
 } from "carbon-components-react";
+import { useNavigate } from "react-router-dom";
 export default function Navigation() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="header-container">
         <HeaderContainer
           render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-            <Header aria-label="nav-bar">
+            <Header aria-label="KAC Platform Name">
               <SkipToContent />
               <HeaderMenuButton aria-label="Open menu" onClick={onClickSideNavExpand} isActive={isSideNavExpanded} />
-              <HeaderName href="/" prefix="K A">
-                [Training Camp]
+              <HeaderName href="#" prefix="KAC">
+                [Platform]
               </HeaderName>
-              <HeaderNavigation aria-label="nav">
+              <HeaderNavigation aria-label="KAC [Platform]">
                 <HeaderMenuItem isCurrentPage href="/">
                   Home
                 </HeaderMenuItem>
                 <HeaderMenuItem href="/athletes">Athletes</HeaderMenuItem>
                 <HeaderMenuItem href="/blog">Blog</HeaderMenuItem>
                 <HeaderMenuItem href="/contacts">Contacts</HeaderMenuItem>
-                <HeaderMenuItem href="/admin">Admin Login</HeaderMenuItem>
+                <HeaderMenuItem href="/admin">Admin</HeaderMenuItem>
               </HeaderNavigation>
+              <SideNav aria-label="Side navigation" expanded={isSideNavExpanded} isPersistent={false}>
+                <SideNavItems>
+                  <HeaderSideNavItems>
+                    <HeaderMenuItem href="/">Home</HeaderMenuItem>
+                    <HeaderMenuItem href="/athletes">Athletes</HeaderMenuItem>
+                    <HeaderMenuItem href="/blog">Blog</HeaderMenuItem>
+                    <HeaderMenuItem href="/contacts">Contacts</HeaderMenuItem>
+                    <HeaderMenuItem href="admin">Admin</HeaderMenuItem>
+                  </HeaderSideNavItems>
+                </SideNavItems>
+              </SideNav>
             </Header>
           )}
         />
