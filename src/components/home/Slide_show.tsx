ab38f40aabbb80bbb77eaaca076d1a5db792b820
Eric <ericthedev@gmail.com>
2022-05-12 17:04:05 -0400

    fix(slideshow): improve slideshow

--- src/components/home/Slide_show.tsx
+++ src/components/home/Slide_show.tsx
@@ -1,25 +1,13 @@
 import React from "react";
-import { Slide } from "react-slideshow-image";
 import "./Slide_show.scss";
 
-const slideImages = [
-  { url: "./login.jpg", caption: "slide 1" },
-  { url: "https://unsplash.com/photos/atSaEOeE8Nk", caption: "slide 2" },
-  { url: "https://unsplash.com/photos/_A7WLos9RfU", caption: "slide 3" },
-  { url: "https://unsplash.com/photos/atSaEOeE8Nk", caption: "slide 4" },
-];
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [{ url: "./image2.jpeg" }, { url: "images/2.jpg" }, { url: "images/3.jpg" }];
 export default function Slide_show() {
   return (
     <div className="slide-container">
-      <Slide>
-        {slideImages.map((slideImage, index) => (
-          <div className="each-slide" key={index}>
-            <div style={{ backgroundImage: `url(${slideImage.url})` }}>
-              <span>{slideImage.caption}</span>
-            </div>
-          </div>
-        ))}
-      </Slide>
+      <SimpleImageSlider width={1000} height={504} images={images} showBullets={true} showNavs={true} />
     </div>
   );
 }
